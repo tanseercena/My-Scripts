d9f55c9c26e4fd884b3ef93edd0c7787bfe3345e
tanseercena <tanseercena@gmail.com>
2017-04-25 14:39:12 +0500

    Dynamic columns

--- excel_index.php
+++ excel_index.php
@@ -1,44 +1,35 @@
 <?php
-/* manage excel index */
-    function getColumn($prevColumn){
-        if(strlen($prevColumn)==1&&$prevColumn!="Z")
-            { $prevColumn++;return $prevColumn; }
-        
-            $colum = '';
-            $list = str_split($prevColumn);
-        
-        if(count($list)==1&&$list[0]=='Z')
-        {
-            $list[0] = "A";
-            $list[1] = "A";
-        }
-        else if(count($list)==2&&$list[1]!='Z')
-        {
-            $list[1]++;
-        }else if(count($list)==2&&$list[1]=='Z'&&$list[0]=='E')
-        {
-                $list[0] = "F";
-                $list[1] = "A";
-        }
-        else if(count($list)==2&&$list[1]=='Z'&&$list[0]=='D')
-        {
-                $list[0] = "E";
-                $list[1] = "A";
-        }
-        else if(count($list)==2&&$list[1]=='Z'&&$list[0]=='C')
-        {
-                $list[0] = "D";
-                $list[1] = "A";
-        }
-        else if(count($list)==2&&$list[1]=='Z'&&$list[0]=='B')
-        {
-                $list[0] = "C";
-                $list[1] = "A";
-        }
-        else if(count($list)==2&&$list[1]=='Z')
-        {
-                $list[0] = "B";
-                $list[1] = "A";
-        }
-        return implode('',$list);
+/* manage excel index 
+*   Example -> getNextColumn('A') => return B
+*   getNextColumn('AZ') => return BA  
+*/
+   function getNextColumn($prevColumn = ''){
+    if($prevColumn == ''){
+        return 'A';
     }
+    if(strlen($prevColumn)==1&&$prevColumn!="Z"){
+        $prevColumn++;
+        return $prevColumn; 
+    }
+    
+    
+    $colum = '';
+    $list = str_split($prevColumn);
+    
+    if(count($list)==1&&$list[0]=='Z')
+    {
+        $list[0] = "A";
+        $list[1] = "A";
+    }
+    else if(count($list)==2&&$list[1]!='Z')
+    {
+        $list[1]++;
+    }else if(count($list)==2&&$list[1]=='Z')
+    {
+            $list[0]++;
+            $list[1] = "A";
+    }
+    return implode('',$list);
+}
+
+
